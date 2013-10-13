400dea32aed1a94774f9eb6ddb21aaa863c217de
Keijiro Takahashi <keijiro@MSX.local>
2013-10-13 10:49:27 +0900

    Fixed wrong UV coordinate.

--- PrimitiveMeshAssetCreator.cs
+++ PrimitiveMeshAssetCreator.cs
@@ -43,10 +43,10 @@ public static class PrimitiveMeshAssetCreator
         mesh.vertices = vertices;
 
         var uvs = new Vector2 [8];
-        uvs [0] = uvs [5] = new Vector2 (-1, -1);
-        uvs [1] = uvs [4] = new Vector2 (+1, -1);
-        uvs [2] = uvs [7] = new Vector2 (-1, +1);
-        uvs [3] = uvs [6] = new Vector2 (+1, +1);
+        uvs [0] = uvs [5] = new Vector2 (0, 0);
+        uvs [1] = uvs [4] = new Vector2 (1, 0);
+        uvs [2] = uvs [7] = new Vector2 (0, 1);
+        uvs [3] = uvs [6] = new Vector2 (1, 1);
         mesh.uv = uvs;
 
         var indices = new int[] {0, 1, 2, 3, 2, 1, 4, 5, 6, 7, 6, 5};
@@ -68,10 +68,10 @@ public static class PrimitiveMeshAssetCreator
         mesh.vertices = vertices;
         
         var uvs = new Vector2 [4];
-        uvs [0] = new Vector2 (-1, -1);
-        uvs [1] = new Vector2 (+1, -1);
-        uvs [2] = new Vector2 (-1, +1);
-        uvs [3] = new Vector2 (+1, +1);
+        uvs [0] = new Vector2 (0, 0);
+        uvs [1] = new Vector2 (1, 0);
+        uvs [2] = new Vector2 (0, 1);
+        uvs [3] = new Vector2 (1, 1);
         mesh.uv = uvs;
         
         var indices = new int[] {0, 1, 3, 2, 0};
@@ -94,10 +94,10 @@ public static class PrimitiveMeshAssetCreator
         mesh.vertices = vertices;
         
         var uvs = new Vector2 [8];
-        uvs [0] = uvs [5] = new Vector2 (-1, -1);
-        uvs [1] = uvs [4] = new Vector2 (+1, -1);
-        uvs [2] = uvs [7] = new Vector2 (-1, +1);
-        uvs [3] = uvs [6] = new Vector2 (+1, +1);
+        uvs [0] = uvs [5] = new Vector2 (0, 0);
+        uvs [1] = uvs [4] = new Vector2 (1, 0);
+        uvs [2] = uvs [7] = new Vector2 (0, 1);
+        uvs [3] = uvs [6] = new Vector2 (1, 1);
         mesh.uv = uvs;
         
         var indices = new int[] {0, 1, 2, 3, 2, 1, 4, 5, 6, 7, 6, 5};
