3465029525fcb6d6dd47accb7d13a254ee6fac1e
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-10-28 07:53:02 +0900

    Recommited the test project.

---
+++ Assets/Script/SimpleRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotation : MonoBehaviour
+{
+    void Update ()
+    {
+        transform.localRotation =
+            Quaternion.AngleAxis (Time.time * 120.0f, Vector3.up) *
+            Quaternion.AngleAxis (Time.time * 33.7f, Vector3.right);
+    }
+}
